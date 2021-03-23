52181eee41a9a56bc96e39777e1f6dddf7da2ca0
Kyohei Uto <kyoheiu@outlook.com>
2021-03-23 09:35:44 +0900

    Fix Nim-code

--- dotfiles.nim
+++ dotfiles.nim
@@ -7,16 +7,21 @@ for kind, path in walkDir(dotfiles):
   let (dir, name, ext) = path.splitFile
   if name == "dotfiles": continue
   else:
-    if kind == pcFile:
+    case kind
+
+    of pcFile:
       let target1 = home & name & ext
       removeFile(target1)
       path.createSymlink(target1)
-    if kind == pcDir:
+
+    of pcDir:
       if name != ".git":
-        for fkind, fpath in walkDir(path):
-          let (fdir, fname, fext) = fpath.splitFile
-          if fname != "colors":
-            let symname = name & "/" & fname & fext
-            let target2 = home & ".config/" & symname
+        for ckind, cpath in walkDir(path):
+          let (cdir, cname, cext) = cpath.splitFile
+          if cname != "colors":
+            let configName = name & "/" & cname & cext
+            let target2 = home & ".config/" & configName
             removeFile(target2)
-            fpath.createSymlink(target2)
+            cpath.createSymlink(target2)
+    
+    else: continue
